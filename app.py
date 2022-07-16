9a6059bd9962263a2dfa7c3e0caaabce7ee62d00
mattyparty <matthewpewewardy@gmail.com>
2022-07-15 22:54:43 -0500

    epub loop

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import ebooklib
 from ebooklib import epub
+##from bs4 import BeautifulSoup
 
 epub_file_path = './data/epubFile.epub'
 
-book = epub.read_epub(epub_file_path )
-items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+def epub2thtml(epub_path):
+    book = epub.read_epub(epub_path)
+    chapters = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapters.append(item.get_content())
+    return chapters
 
-print(items)
+test = epub2thtml(epub_file_path) 
+
+print(test)
